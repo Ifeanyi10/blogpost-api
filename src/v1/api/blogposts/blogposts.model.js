d5b24503828f8cf9e864d5ab7ddc6317fc2560aa
Ifeanyi <ifeanyiodenigbo10@gmail.com>
2020-05-26 20:59:58 +0100

    add blog post model

---
+++ src/v1/api/blogposts/blogposts.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+let  Schema = mongoose.Schema
+
+//let roles = ['guest', 'admin', 'moderator']
+
+const blogpostSchema = new Schema(
+    {
+        title: {type: String},
+        content: {type: String},
+        comments: [{type: Schema.ObjectId, ref: 'Comments'}]
+    }
+)
+
+module.exports = mongoose.model("Users", blogpostSchema)
